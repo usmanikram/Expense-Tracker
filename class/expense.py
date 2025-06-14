fe8a0a19a03d16981cb5f1b9ff2725408877a906
Usman <usmanikram.ch@gmail.com>
2025-06-14 20:27:20 +0500

    File Structure

---
+++ class/expense.py
@@ -0,0 +1,8 @@
+class Expense:
+    def __init__(self,id,description,amount,date,category):
+        self.id = id
+        self.description = description
+        self.amount = amount
+        self.date = date
+        self.category = category
+
